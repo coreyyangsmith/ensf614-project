b5094d61641ae0f15314ef2f1433e0e59a12892d
Yajur Vashisht <132369766+yvashisht@users.noreply.github.com>
2023-11-13 18:08:12 -0700

    Added payment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { StripeProvider } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
+
+const stripePromise = loadStripe('pk_test_51OC8jgCQlBoyLNaWHKEXgvdaYgzAviXr6hWXuS1W4ABUyFLPU1rGZ0PLFIV5fTGqGuLDSzxIsDMb4VLMKTey2SM600RX3nqvf1');
+
+function App() {
+    return (
+        <StripeProvider stripe={stripePromise}>
+            <MyStore />
+        </StripeProvider>
+    );
+}
