09465933f857fe019888c2a3c4138d28d4d360cf
Atif Pathan <atifkhan.p1@gmail.com>
2023-11-21 18:54:38 -0500

    Adding Login authentication stuff in main

--- main/serializers.py
+++ main/serializers.py
@@ -17,6 +17,7 @@
 #   IMPORTS
 #-------------------------------------------------------#
 
+
 # Django Imports
 from .models import *
 import datetime
@@ -26,6 +27,8 @@ from django.contrib.auth.password_validation import validate_password
 
 # Django Rest Framework (DRF) Imports
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 
 
 #   SERIALIZERS
@@ -71,3 +74,39 @@ class PassengerSerializer(serializers.ModelSerializer):
         flight_id = FlightSerializer()
         model = Passenger
         fields = '__all__'
+
+# Add these serializers for user registration and login - Atif
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email")
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
